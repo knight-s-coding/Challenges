6fec7be40cc397c59f3ab18a5980ba8b54c1ad4b
Antonio <ant.iatonna@gmail.com>
2020-02-20 11:42:02 -0500

    Demo

---
+++ Python #1/demo.py
@@ -0,0 +1,13 @@
+def function():
+    print("Hello World")
+
+    name = 5
+    stringName = int(input("What would you like to say?"))
+
+    #if statement
+    if(name == 5):
+        print("Yes")
+    elif(name == 2):
+        print("maybe")
+    else:
+        print("No")
